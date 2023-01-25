2685e882bcaa93ea31c9edde78e1a4900ef4f73a
tomatoo <aidannwilhite@gmail.com>
2023-01-25 14:54:06 -0600

    Update Instrumentor.cpp

--- puffin/src/Debug/Instrumentor.cpp
+++ puffin/src/Debug/Instrumentor.cpp
@@ -1,37 +1,35 @@
-
 #include "Instrumentor.h"
 
 namespace puffin
 {
     Debug *Debug::s_debug = nullptr;
 
-    void Debug::writeFunction(std::string name, std::string file, float start, float elapsed)
+    uint64_t micros()
+    {
+        uint64_t us = std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::high_resolution_clock::now().time_since_epoch())
+                .count();
+        return us; 
+    }
+
+    void Debug::writeFunction(std::string name, std::string file, uint64_t start, uint64_t elapsed)
     {
         std::stringstream json;
 
         json << std::setprecision(3) << std::fixed;
-        json << ",{";
+        json << "\n,{";
         json << "\"cat\":\"function\",";
-        json << "\"dur\":" << elapsed << ',';
+        json << "\"dur\":" << elapsed<< ',';
         json << "\"name\":\"" << name << "\",";
         json << "\"ph\":\"X\",";
-        json << "\"pid\":0,";
-        json << "\"tid\":" << s_pid << ",";
-        json << "\"ts\":" << start;
-        json << "}\n";
+        json << "\"pid\":" << m_subsectionPid << ",";
+        json << "\"tid\":" << 0 << ",";
+        json << "\"ts\":" << (start - m_debugStartTime);
+        json << "}";
 
-        printf("%i pid\n", s_pid);
+        printf("%i pid\n", m_subsectionPid);
 
         m_output << json.str();
         m_output.flush();
     }
-
-    Proccess::Proccess(std::string fileName, std::string functionName)
-    {
-        // Profiles how long the proccess takes
-        // Puts it on the base proccess pid (0)
-        m_profile = new Profile(fileName, functionName);
-        s_pid += 1;
-    }
-
 } // namespace puffin
